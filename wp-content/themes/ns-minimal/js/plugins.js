3a0dea3b7b8e12273344e7f6ba44f629346bf8e9
Frank Jantunen <50525647+fjantunen@users.noreply.github.com>
2020-08-29 15:08:11 -0400

    WP2Static CREATE wp-content/themes/ns-minimal/js/plugins.js

---
+++ wp-content/themes/ns-minimal/js/plugins.js
@@ -0,0 +1,80 @@
+/**
+ * navigation.js
+ *
+ * Handles toggling the navigation menu for small screens.
+ */
+( function() {
+	var container, button, menu;
+
+  var hamburger = document.querySelector(".hamburger");
+
+	container = document.getElementById( 'site-navigation' );
+	if ( ! container )
+		return;
+
+	button = container.getElementsByTagName( 'button' )[0];
+	if ( 'undefined' === typeof button )
+		return;
+
+	menu = container.getElementsByTagName( 'ul' )[0];
+
+	// Hide menu toggle button if menu is empty and return early.
+	if ( 'undefined' === typeof menu ) {
+		button.style.display = 'none';
+		return;
+	}
+
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) )
+		menu.className += ' nav-menu';
+
+	button.onclick = function() {
+		if ( -1 !== container.className.indexOf( 'toggled' ) )
+			container.className = container.className.replace( ' toggled', '' );
+		else
+			container.className += ' toggled';
+
+      hamburger.classList.toggle("is-active");
+	};
+
+
+  // Look for .hamburger
+  // var hamburger = document.querySelector(".hamburger");
+  // // On click
+  // hamburger.addEventListener("click", function() {
+  //   // Toggle class "is-active"
+  //   hamburger.classList.toggle("is-active");
+  //   // Do something else, like open/close menu
+  // });
+
+
+} )();
+
+( function() {
+	var container, button, menu;
+
+	container = document.getElementById( 'top-navigation' );
+	if ( ! container )
+		return;
+
+	button = container.getElementsByTagName( 'button' )[0];
+	if ( 'undefined' === typeof button )
+		return;
+
+	menu = container.getElementsByTagName( 'ul' )[0];
+
+	// Hide menu toggle button if menu is empty and return early.
+	if ( 'undefined' === typeof menu ) {
+		button.style.display = 'none';
+		return;
+	}
+
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) )
+		menu.className += ' nav-menu';
+
+	button.onclick = function() {
+		if ( -1 !== container.className.indexOf( 'toggled' ) )
+			container.className = container.className.replace( ' toggled', '' );
+		else
+			container.className += ' toggled';
+	};
+} )();
